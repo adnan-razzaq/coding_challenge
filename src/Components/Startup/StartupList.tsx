99c209f385c381ea58239f43b52b4ab8abb32434
Adnan Razzaq <adnan.razzaq@stud.uni-bamberg.de>
2022-02-22 18:24:27 +0100

    Removig unused components

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,22 +1,10 @@
 import { Fragment, ReactElement, useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { StartupHttpService } from "../../Http/Startup/Startup.http.service";
 import Paper from "@mui/material/Paper";
 import { Startup } from "../../Types/Startup";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
 // @ts-ignore
 export default function StartupList(): ReactElement {
   const [startUpData, setstartUpData] = useState<Startup[]>([]);
@@ -35,7 +23,7 @@ export default function StartupList(): ReactElement {
   return (
     <>
       <h1>Hello from componenet</h1>
-      /*{" "}
+
       {startUpData.map(
         (item: {
           name: string;
